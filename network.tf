bb8293b00299621d6bbf2172caa6bc5ad4c05ea6
Bhavleen Kaur <bhavleen1995@gmail.com>
2023-06-15 07:16:54 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/bhavleen1995-org/network/azurerm"
   version = "3.5.0"
-  resource_group_name = azurerm_resource_group.myresourcegroup.bhavleenkaur-workshop
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
