87c67e7eeec0eca3457ab5a66cac9ffeda92cf0e
Bhavleen Kaur <bhavleen1995@gmail.com>
2023-06-15 07:21:26 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -203,3 +203,8 @@ resource "null_resource" "configure-cat-app" {
     }
   }
 }
+module "network" {
+  source  = "app.terraform.io/bhavleen1995-org/network/azurerm"
+  version = "3.5.0"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+}
